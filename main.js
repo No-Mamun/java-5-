32ec6fe8aed50df287ac4ad354ca54d0b648a6cd
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2024-01-17 07:21:56 +0600

    Add files via upload
    java-5

---
+++ main.js
@@ -0,0 +1,12 @@
+let users = ['Nasim','Shuvo','Sadik','Rabbi','Shamin','Likhon','Nasim','Shuvo','Sadik','Rabbi','Shamin','Likhon','Nasim','Shuvo','Sadik','Rabbi','Shamin','Likhon',
+'Nasim','Shuvo','Sadik','Rabbi','Shamin','Likhon','Nasim','Shuvo','Sadik','Rabbi','Shamin','Likhon','Nasim','Shuvo','Sadik','Rabbi','Shamin',
+'Likhon',];
+
+let arrLen = users.length;
+
+let counter;
+
+for (counter = 0; counter < arrLen; counter ++){
+
+              console.log(users [counter]);
+}
